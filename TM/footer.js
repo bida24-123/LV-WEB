b91d6dbfff748bf763b226dff1cf42d5a88dfb87
bida24-123 <bida24-123@thuto.bac.ac.bw>
2025-05-24 15:57:46 +0200

    Create footer.js

---
+++ TM/footer.js
@@ -0,0 +1,64 @@
+function footer() {
+    return `
+      <div id="futer_footer">
+        <div id="main_footer_div">
+          <div id="footer_hr_upper">
+            <div>
+              <div>
+                <h3 class="find_a_store_footer"><a href="https://www.louisvuitton.com/eng-us/stores" target="_blank">FIND A STORE</a></h3>
+                <h3 class="find_a_store_footer"><a href="https://us.louisvuitton.com/eng-us/mylv" target="_blank">BECOME A MEMBER</a></h3>
+                <h3 class="find_a_store_footer"><a href="https://us.louisvuitton.com/eng-us/mylv/signup" target="_blank">SIGN UP FOR EMAIL</a></h3>
+                <h3 class="find_a_store_footer"><a href="https://us.louisvuitton.com/eng-us/services/student-offer" target="_blank">STUDENT DISCOUNTS</a></h3>
+                <h3 class="find_a_store_footer"><a href="mailto:clientservices@louisvuitton.com">SEND US FEEDBACK</a></h3>
+              </div>
+  
+              <div>
+                <h3 id="footer_get_help_h3">GET HELP</h3>
+                <p id="footer_get_help_p">
+                  <a href="https://us.louisvuitton.com/eng-us/myorders" target="_blank">Order Status</a><br>
+                  <a href="https://us.louisvuitton.com/eng-us/services/delivery" target="_blank">Delivery</a><br>
+                  <a href="https://us.louisvuitton.com/eng-us/services/returns" target="_blank">Returns</a><br>
+                  <a href="https://us.louisvuitton.com/eng-us/services/payment" target="_blank">Payment Options</a><br>
+                  <a href="mailto:clientservices@louisvuitton.com">Contact Us on LV.com Inquiries</a><br>
+                  <a href="mailto:help@louisvuitton.com">Contact Us on Other Inquiries</a>
+                </p>
+              </div>
+  
+              <div>
+                <h3 id="footer_about_nike_h3">ABOUT LOUIS VUITTON</h3>
+                <p id="footer_about_nike_p">
+                  <a href="https://us.louisvuitton.com/eng-us/magazine" target="_blank">News</a><br>
+                  <a href="https://jobs.louisvuitton.com" target="_blank">Careers</a><br>
+                  <a href="https://www.lvmh.com/investors/" target="_blank">Investors</a><br>
+                  <a href="https://us.louisvuitton.com/eng-us/sustainability" target="_blank">Sustainability</a>
+                </p>
+              </div>
+            </div>
+  
+            <div>
+              <a href="https://twitter.com/LouisVuitton" target="_blank"><img src="https://img.icons8.com/material/2x/twitter-squared.png" class="social_img" /></a>
+              <a href="https://www.facebook.com/LouisVuitton" target="_blank"><img src="https://cdn-icons-png.flaticon.com/128/20/20837.png" class="social_img" /></a>
+              <a href="https://www.youtube.com/user/louisvuitton" target="_blank"><img src="https://img.icons8.com/material/2x/youtube--v1.png" class="social_img" /></a>
+              <a href="https://www.instagram.com/louisvuitton/" target="_blank"><img src="https://img.icons8.com/windows/2x/instagram.png" class="social_img" /></a>
+            </div>
+          </div>
+  
+          <hr style="background-color: gray; height: 0px;" />
+  
+          <div id="footer_hr_lower">
+            <div>
+              <img src="https://i2.paste.pics/44be9d50d7b0b86c3d77e3bf2450dac7.png" id="footer_location_pin_image" />
+              <pre id="footer_location_india">Worldwide  <span style="color: gray;">© 2025 Louis Vuitton. All Rights Reserved</span></pre>
+            </div>
+            <div>
+              <pre style="color: gray; font-size: x-small; font-family: Arial, Helvetica, sans-serif;">
+    Guides   Terms of Sale   Terms of Use   Privacy Policy</pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+  
+  export default footer;
+  
